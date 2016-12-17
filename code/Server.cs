c401ca20dd073675d95a616eb23dc80079e5ba1a
frozenk1458 <frozenk@hotmail.fr>
2016-12-17 07:59:22 +0100

    Add files via upload

---
+++ code/Server.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Threading;
+using System.IO;
+using System.Globalization;
+
+namespace QuickSharp
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            //Initialize the IP address and port manually for now. I replace in this code the IP address text chain by ServerAddress. The program could be configured to know automatically the machine ip address.
+            string ipAddress = ServerAddress;
+            int port = 8000;
+            //Initialize the buffer for TCP connection
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(" ");
+            //Log sentences to write down into the console the connection address and port
+            Console.WriteLine("IP="+ipAddress + ":" + port);
+            //Socket initialization
+            Socket S = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            Socket list = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            //Binding the socket to the IP address and port previously defined
+            IPAddress ipClient;
+            IPAddress.TryParse(ipAddress, out ipClient);
+            IPEndPoint ipe = new IPEndPoint(ipClient, port);
+            S.Bind(ipe);
+            //Setup the socket to listen on port defined before. Initialize the length of queue at 10.
+            S.Listen(10);
+            //Loop to accept clients connections (simultaneous connections is possible). Exchange with the client and then close the client connection.
+            while(true)
+            {
+                list=S.Accept();
+                //Initialize the reception buffer
+                string c = " ";
+                while(true)
+                {
+                    //Read the buffer
+                    list.Receive(bytes);
+                    //Text received chararcter by character. In the future we plan to try an exchange of other orders.
+                    string t = System.Text.Encoding.UTF8.GetString(bytes);
+                    //We put an character '0' on the client side to define a text end. If we receive a '0' character we stop reading the buffer.
+                    //This part would be improved.
+                    if(String.Compare(t,"0") == 0){ break;}
+                    //Rebuild initial text character by character.
+                    c = c + t;
+                }
+                //Write down the complete text received
+                Console.WriteLine(c);
+                //Close the client connection
+                list.Close();
+            }
+            //S.Close();
+          }
+     }
+}
---
+++ code/client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Threading;
+using System.IO;
+
+namespace DefaultNamespace
+{
+     class MainClass
+	{
+	 public static void Main(string[] args)
+	 {
+        //Initialize the sending buffer
+        byte[] chaine = System.Text.Encoding.UTF8.GetBytes("Testp0");
+        /*Testing part
+        string s = System.Text.Encoding.UTF8.GetString(chaine);
+        Console.Write(s);
+        */
+        //Initialize the socket
+		Socket ClientServer = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+        //Connect to the server on listened address and port.I replace in this code the IP address text chain by ServerAddress. The program could be configured to know automatically the machine ip address.
+        ClientServer.Connect(ServerAddress,8000);
+        //Send text. One letter at a time. It would be improved in various ways.
+        ClientServer.Send(chaine, SocketFlags.None);
+
+        //ClientServer.Send(chaine);
+        //ClientServer.Close();
+	 }
+    }
+}
