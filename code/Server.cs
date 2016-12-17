df8240ececb80bf2ca7705fc5b79d2d9ef33928d
frozenk1458 <frozenk@hotmail.fr>
2016-12-17 15:51:14 +0100

    Update Server.cs

--- code/Server.cs
+++ code/Server.cs
@@ -10,7 +10,7 @@ namespace DefaultNamespace
 {
     class MainClass
     {
-        private static void Main()
+        public static void Main(string[] args)
         {
             //Get the current host name from the system
             String strHostName = string.Empty;
