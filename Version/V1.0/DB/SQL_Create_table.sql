9f6abd9109721444c19876131c5b179997aaa7d1
frozenk1458 <frozenk@hotmail.fr>
2016-12-25 20:28:22 +0100

    Update SQL_Create_table.sql

--- Version/V1.0/DB/SQL_Create_table.sql
+++ Version/V1.0/DB/SQL_Create_table.sql
@@ -3,6 +3,6 @@ use SoftCSharp;
 CREATE TABLE users  
 ( 
  id INT NOT NULL IDENTITY(0, 1) PRIMARY KEY, 
- logi VARCHAR(20) NULL,
- passw VARCHAR(20) NULL  
+ logi VARCHAR(20),
+ passw VARCHAR(20)   
  );
